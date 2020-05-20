6648ecdb8854f77665d8644083a651d04b03f1a3
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-21 00:09:40 +0800

    add uniq validation to cocktail & ingredient in dose model

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,8 @@
 class Dose < ApplicationRecord
   # validation
   validates :description, presence: true
-  validates :ingredient, presence: true
-  validates :cocktail, presence: true
+  validates :ingredient, presence: true, uniqueness: { scope: :cocktail }
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
 
   # relationship
   belongs_to :ingredient
