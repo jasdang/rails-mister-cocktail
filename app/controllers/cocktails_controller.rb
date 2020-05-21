ee6fed44666b7195bf085b2c05b70f797ec836e9
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-21 22:29:06 +0800

    cocktail new & create done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,4 +6,23 @@ class CocktailsController < ApplicationController
   def show
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_param)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_param
+    params.require(:cocktail).permit(:name)
+  end
 end
