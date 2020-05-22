17076af2fbf76eef869b5811e420de866cba6369
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-22 21:36:50 +0800

    dose destroy done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -15,6 +15,9 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(params[:cocktail_id])
   end
 
   private
