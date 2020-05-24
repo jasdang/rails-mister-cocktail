3d81b7707023ee2a22d5a62740bf52d2fb3cd30b
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-24 15:13:57 +0800

    root to cocktail index and update cocktail create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  root to: 'cocktails#index', as: :cocktails
+  post '/', to: 'cocktails#create'
+  resources :cocktails, only: [:show, :new] do
     resources :doses, only: [:new, :create, :destroy]
   end
 end
