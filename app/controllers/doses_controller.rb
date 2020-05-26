51384e80fe0339302b3687d3f625cd2ae5c1f26e
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-26 20:40:56 +0800

    rake

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,15 @@
 class DosesController < ApplicationController
-  before_action :set_instances, only: [:new, :create]
 
   def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
   end
 
   def create
-    @dose.description = params[:dose][:description]
-    @dose.ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(params[:cocktail_id])
     else
@@ -17,14 +20,12 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(params[:cocktail_id])
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
 
-  def set_instances
-    @dose = Dose.new()
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @cocktail
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
